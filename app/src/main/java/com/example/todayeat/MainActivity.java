f2a2985dccf84c201172fe6778c05342dfc5126e
kimminsub <msmk530@naver.com>
2019-05-11 15:17:37 +0900

    리스트버튼 눌렀을때 리스트창

--- app/src/main/java/com/example/todayeat/MainActivity.java
+++ app/src/main/java/com/example/todayeat/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.todayeat;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
         mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
         sliderAdapter = new SliderAdapter(this);
@@ -30,7 +36,22 @@ public class MainActivity extends AppCompatActivity {
         addDotsIndicator(2);
 
         mViewPager.addOnPageChangeListener(viewListener);
+        Button b = (Button)findViewById(R.id.button4);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        MyTwo.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+
+
     }
+
+
 
     public void addDotsIndicator(int position){
         mDotLayout.removeAllViews();
