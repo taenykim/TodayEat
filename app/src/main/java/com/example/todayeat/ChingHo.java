ca897baba8745bcf3695559b2056fd42a2f3393e
kimminsub <msmk530@naver.com>
2019-05-13 12:07:20 +0900

    칭호변경 추가

--- app/src/main/java/com/example/todayeat/ChingHo.java
+++ app/src/main/java/com/example/todayeat/ChingHo.java
@@ -24,10 +24,30 @@ public class ChingHo extends AppCompatActivity {
 
         //액티비티 시작!
         startActivity(intent);
+        Toast.makeText(this,"칭호가 변경되었습니다.",Toast.LENGTH_LONG).show();
+    }
+    public void onClick2(View view){
+        Intent intent = new Intent(this, MyCharacter.class);
+        intent.putExtra("Chingho1","결정장애 중기"); //키 - 보낼 값(밸류)
 
+        //액티비티 시작!
+        startActivity(intent);
+        Toast.makeText(this,"칭호가 변경되었습니다.",Toast.LENGTH_LONG).show();
+    }
+    public void onClick3(View view){
+        Intent intent = new Intent(this, MyCharacter.class);
+        intent.putExtra("Chingho1","결정장애 말기"); //키 - 보낼 값(밸류)
 
+        //액티비티 시작!
+        startActivity(intent);
+        Toast.makeText(this,"칭호가 변경되었습니다.",Toast.LENGTH_LONG).show();
+    }
+    public void onClick4(View view){
+        Intent intent = new Intent(this, MyCharacter.class);
+        intent.putExtra("Chingho1","결정장애 그자체"); //키 - 보낼 값(밸류)
 
-
+        //액티비티 시작!
+        startActivity(intent);
         Toast.makeText(this,"칭호가 변경되었습니다.",Toast.LENGTH_LONG).show();
     }
 }
