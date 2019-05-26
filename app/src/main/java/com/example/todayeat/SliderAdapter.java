70ca90fb438a28932a4ec4b50854e7d93254554d
TAENY-KIM <kimtaeeeny@naver.com>
2019-05-26 23:43:34 +0900

    랜덤구현(슬라이드 조건부 랜덤 ok)

--- app/src/main/java/com/example/todayeat/SliderAdapter.java
+++ app/src/main/java/com/example/todayeat/SliderAdapter.java
@@ -27,6 +27,7 @@ import java.util.Map;
 import java.util.Random;
 
 import static com.example.todayeat.ListActivity.arrayData;
+import static com.google.firebase.database.FirebaseDatabase.getInstance;
 
 public class SliderAdapter extends PagerAdapter {
     Context context;
@@ -41,6 +42,7 @@ public class SliderAdapter extends PagerAdapter {
     String name;
     long age;
     String gender = "";
+    int po;
 
 
     public SliderAdapter(Context context){
@@ -75,7 +77,7 @@ public class SliderAdapter extends PagerAdapter {
     }
 
     @Override
-    public Object instantiateItem(ViewGroup container, int position){
+    public Object instantiateItem(ViewGroup container, final int position){
         layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.slide_layout,container,false);
 
@@ -84,25 +86,132 @@ public class SliderAdapter extends PagerAdapter {
         slideHeading.setTextColor(Color.WHITE);
         Button btn = (Button) view.findViewById(R.id.button);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        po=position;
 
-                //0 >> 분류
-                //1 >> 가게이름
-                //2 >> 전화번호
-                //3 >> 대표메뉴
-                Intent intent = new Intent(v.getContext(), DetailActivity.class);
-                Random random = new Random();
-                String[] tempData = arrayData.get(random.nextInt(6)).split("\\s+");
+        switch (po){
+            case 0:
 
-                intent.putExtra("name",tempData[1]);
-                intent.putExtra("number",tempData[2]);
-                intent.putExtra("menu",tempData[3]);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-                v.getContext().startActivity(intent);
-            }
-        });
+                        //0 >> 분류
+                        //1 >> 가게이름
+                        //2 >> 전화번호
+                        //3 >> 대표메뉴
+
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                        Random random = new Random();
+                        String[] tempData = arrayData.get(random.nextInt(2)+2).split("\\s+");
+
+                        intent.putExtra("name",tempData[1]);
+                        intent.putExtra("number",tempData[2]);
+                        intent.putExtra("menu",tempData[3]);
+
+                        v.getContext().startActivity(intent);
+                    }
+                });
+
+                break;
+            case 1:
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //0 >> 분류
+                        //1 >> 가게이름
+                        //2 >> 전화번호
+                        //3 >> 대표메뉴
+
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                        Random random = new Random();
+                        String[] tempData = arrayData.get(random.nextInt(1)+4).split("\\s+");
+
+                        intent.putExtra("name",tempData[1]);
+                        intent.putExtra("number",tempData[2]);
+                        intent.putExtra("menu",tempData[3]);
+
+                        v.getContext().startActivity(intent);
+                    }
+                });
+
+                break;
+            case 2:
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //0 >> 분류
+                        //1 >> 가게이름
+                        //2 >> 전화번호
+                        //3 >> 대표메뉴
+
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                        Random random = new Random();
+                        String[] tempData = arrayData.get(random.nextInt(6)).split("\\s+");
+
+                        intent.putExtra("name",tempData[1]);
+                        intent.putExtra("number",tempData[2]);
+                        intent.putExtra("menu",tempData[3]);
+
+                        v.getContext().startActivity(intent);
+                    }
+                });
+
+                break;
+            case 3:
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //0 >> 분류
+                        //1 >> 가게이름
+                        //2 >> 전화번호
+                        //3 >> 대표메뉴
+
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                        Random random = new Random();
+                        String[] tempData = arrayData.get(random.nextInt(2)).split("\\s+");
+
+                        intent.putExtra("name",tempData[1]);
+                        intent.putExtra("number",tempData[2]);
+                        intent.putExtra("menu",tempData[3]);
+
+                        v.getContext().startActivity(intent);
+                    }
+                });
+
+                break;
+            case 4:
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //0 >> 분류
+                        //1 >> 가게이름
+                        //2 >> 전화번호
+                        //3 >> 대표메뉴
+
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                        Random random = new Random();
+                        String[] tempData = arrayData.get(random.nextInt(2)+5).split("\\s+");
+
+                        intent.putExtra("name",tempData[1]);
+                        intent.putExtra("number",tempData[2]);
+                        intent.putExtra("menu",tempData[3]);
+
+                        v.getContext().startActivity(intent);
+                    }
+                });
+
+                break;
+
+            default:
+                break;
+        }
 
         slideImageView.setImageResource(slide_images[position]);
         slideHeading.setText(slide_headings[position]);
@@ -124,7 +233,7 @@ public class SliderAdapter extends PagerAdapter {
     }
 
     public void postFirebaseDatabase(boolean add){
-        mPostReference = FirebaseDatabase.getInstance().getReference();
+        mPostReference = getInstance().getReference();
         Map<String, Object> childUpdates = new HashMap<>();
         Map<String, Object> postValues = null;
         if(add){
@@ -160,7 +269,7 @@ public class SliderAdapter extends PagerAdapter {
                 Log.w("getFirebaseDatabase","loadPost:onCancelled", databaseError.toException());
             }
         };
-        Query sortbyAge = FirebaseDatabase.getInstance().getReference().child("id_list").orderByChild(sort);
+        Query sortbyAge = getInstance().getReference().child("id_list").orderByChild("idx");
         sortbyAge.addListenerForSingleValueEvent(postListener);
     }
 
